632a0eb189f38de0db329f57d378c52904e8057b
Eshu <tjano.xibalba@gmail.com>
2025-11-24 01:00:24 +0900

    BaseDecoder test

--- src/test/scala/h8io/cfg/BaseDecoderTest.scala
+++ src/test/scala/h8io/cfg/BaseDecoderTest.scala
@@ -1,6 +1,56 @@
 package h8io.cfg
 
+import cats.data.Validated
+import cats.implicits.catsSyntaxValidatedIdBinCompat0
+import h8io.cfg.Decoder.Result
+import h8io.cfg.errors.UnexpectedNode
+import h8io.cfg.raw.{Id, Location, Node}
+import h8io.reflect.typeOf
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.Inside
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
-class BaseDecoderTest extends AnyFlatSpec with Matchers {}
+class BaseDecoderTest extends AnyFlatSpec with Matchers with Inside with MockFactory {
+  "BaseDecoder" should "parse scalar if scalar parse method is overridden" in {
+    val decoder = new BaseDecoder[Node.Scalar] {
+      override protected def parse(scalar: Node.Scalar): Result[Node.Scalar] = Validated.Valid(scalar)
+    }
+    val scalar = Node.Scalar(Id.Root, "test scalar", mock[Location])
+    inside(decoder(scalar)) { case Validated.Valid(result) => result should be theSameInstanceAs scalar }
+  }
+
+  it should "not parse scalar if scalar parse method is not overridden" in {
+    val decoder = new BaseDecoder[Any] {}
+    val scalar = Node.Scalar(Id.Root, "test scalar", mock[Location])
+    decoder(scalar) shouldBe UnexpectedNode(scalar, typeOf[Any]).invalidNec
+  }
+
+  it should "parse map if map parse method is overridden" in {
+    val decoder = new BaseDecoder[Node.Map] {
+      override protected def parse(map: Node.Map): Result[Node.Map] = Validated.Valid(map)
+    }
+    val map = mock[Node.Map]
+    inside(decoder(map)) { case Validated.Valid(result) => result should be theSameInstanceAs map }
+  }
+
+  it should "not parse map if map parse method is not overridden" in {
+    val decoder = new BaseDecoder[Any] {}
+    val map = mock[Node.Map]
+    decoder(map) shouldBe UnexpectedNode(map, typeOf[Any]).invalidNec
+  }
+
+  it should "parse seq if seq parse method is overridden" in {
+    val decoder = new BaseDecoder[Node.Seq] {
+      override protected def parse(seq: Node.Seq): Result[Node.Seq] = Validated.Valid(seq)
+    }
+    val map = mock[Node.Seq]
+    inside(decoder(map)) { case Validated.Valid(result) => result should be theSameInstanceAs map }
+  }
+
+  it should "not parse seq if seq parse method is not overridden" in {
+    val decoder = new BaseDecoder[Any] {}
+    val map = mock[Node.Seq]
+    decoder(map) shouldBe UnexpectedNode(map, typeOf[Any]).invalidNec
+  }
+}
