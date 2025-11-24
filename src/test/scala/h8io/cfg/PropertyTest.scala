5c374c512516ad76c0932da89816fd0869b45bb7
Eshu <tjano.xibalba@gmail.com>
2025-11-24 21:44:41 +0900

    decode test

--- src/test/scala/h8io/cfg/PropertyTest.scala
+++ src/test/scala/h8io/cfg/PropertyTest.scala
@@ -1,8 +1,33 @@
 package h8io.cfg
 
+import cats.syntax.all.*
+import h8io.cfg.raw.Node
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
 
-class PropertyTest extends AnyFlatSpec with Matchers with MockFactory with ScalaCheckPropertyChecks {}
+class PropertyTest extends AnyFlatSpec with Matchers with MockFactory with ScalaCheckPropertyChecks {
+  "decode" should "return a valid result returned by decoder" in {
+    implicit val decoder: Decoder[String] = mock[Decoder[String]]
+    val node = mock[Node.Map]
+    (decoder.apply _).expects(node).returning("test result".valid)
+    Property.decode[String](node) shouldBe "test result".valid
+  }
+
+  it should "return an invalid result returned by decoder" in {
+    implicit val decoder: Decoder[String] = mock[Decoder[String]]
+    val node = mock[Node.Seq]
+    val error = mock[Decoder.Error].invalidNec
+    (decoder.apply _).expects(node).returning(error)
+    Property.decode[String](node) shouldBe error
+  }
+
+  it should "return a Thrown result if decoder throws an exception" in {
+    implicit val decoder: Decoder[String] = mock[Decoder[String]]
+    val node = mock[Node.Map]
+    val exception = new RuntimeException("decoder exception")
+    (decoder.apply _).expects(node).throws(exception)
+    Property.decode[String](node) shouldBe Decoder.Thrown(node, exception).invalidNec
+  }
+}
