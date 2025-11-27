636e6130bfad7a7a6601f12d62bfc1f371ab3390
Eshu <tjano.xibalba@gmail.com>
2025-11-27 21:16:22 +0900

    || operator for decoders

--- src/main/scala/h8io/cfg/Decoder.scala
+++ src/main/scala/h8io/cfg/Decoder.scala
@@ -22,16 +22,16 @@ object Decoder {
   implicit val monad: Monad[Decoder] = new Monad[Decoder] {
     override def map[A, B](fa: Decoder[A])(f: A => B): Decoder[B] = fa.andThen(_.map(f))
 
-    override def pure[A](x: A): Decoder[A] = _ => x.valid
+    def pure[A](x: A): Decoder[A] = _ => x.valid
 
-    override def flatMap[A, B](fa: Decoder[A])(f: A => Decoder[B]): Decoder[B] =
+    def flatMap[A, B](fa: Decoder[A])(f: A => Decoder[B]): Decoder[B] =
       node =>
         fa(node) match {
           case Validated.Valid(a) => f(a)(node)
           case invalid @ Validated.Invalid(_) => invalid
         }
 
-    override def tailRecM[A, B](a: A)(f: A => Decoder[Either[A, B]]): Decoder[B] = { node =>
+    def tailRecM[A, B](a: A)(f: A => Decoder[Either[A, B]]): Decoder[B] = { node =>
       @tailrec def loop(current: A): Result[B] =
         f(current)(node) match {
           case Validated.Valid(Right(b)) => b.valid
@@ -43,14 +43,6 @@ object Decoder {
   }
 
   implicit val semigroupK: SemigroupK[Decoder] = new SemigroupK[Decoder] {
-    override def combineK[A](x: Decoder[A], y: Decoder[A]): Decoder[A] =
-      node =>
-        x(node) match {
-          case v @ Validated.Valid(_) => v
-          case Validated.Invalid(xe) => y(node) match {
-              case v @ Validated.Valid(_) => v
-              case Validated.Invalid(ye) => Validated.Invalid(xe ++ ye)
-            }
-        }
+    def combineK[A](x: Decoder[A], y: Decoder[A]): Decoder[A] = x || y
   }
 }
--- src/test/scala/h8io/cfg/DecoderOpsTest.scala
+++ src/test/scala/h8io/cfg/DecoderOpsTest.scala
@@ -1,5 +1,6 @@
 package h8io.cfg
 
+import cats.data.{NonEmptyChain, Validated}
 import cats.syntax.all.*
 import h8io.cfg.raw.Node
 import org.scalamock.scalatest.MockFactory
@@ -41,5 +42,36 @@ class DecoderOpsTest extends AnyFlatSpec with Matchers with MockFactory with Sca
       f.expects(42L).returning(fError)
       composition(node) shouldBe fError
     }
+  }
+
+  "||" should "return the left decoder result when the left decoder succeeds" in {
+    val left = mock[Decoder[String]]("left decoder")
+    val right = mock[Decoder[String]]("right decoder")
+    val decoder = left || right
+    val node = mock[Node.Map]
+    (left.apply _).expects(node).returning("left result".valid)
+    decoder(node) shouldBe "left result".valid
+  }
+
+  it should "accumulate errors from both decoders when both fail" in {
+    val left = mock[Decoder[String]]("left decoder")
+    val right = mock[Decoder[String]]("right decoder")
+    val decoder = left || right
+    val node = mock[Node.Map]
+    (left.apply _).expects(node).returning(mock[CfgError].invalidNec)
+    (right.apply _).expects(node).returning("right result".valid)
+    decoder(node) shouldBe "right result".valid
+  }
+
+  it should "return the combined error if both decoders' results are invalid" in {
+    val left = mock[Decoder[String]]("left decoder")
+    val right = mock[Decoder[String]]("right decoder")
+    val decoder = left || right
+    val node = mock[Node.Map]
+    val leftError = NonEmptyChain(mock[CfgError])
+    val rightError = NonEmptyChain(mock[CfgError])
+    (left.apply _).expects(node).returning(leftError.invalid)
+    (right.apply _).expects(node).returning(rightError.invalid)
+    decoder(node) shouldBe Validated.Invalid(leftError ++ rightError)
   }
 }
