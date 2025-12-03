25a93e6581bef3d1ce79cc6e29fa8f0ef4391c62
Eshu <tjano.xibalba@gmail.com>
2025-12-03 20:04:30 +0900

    Implicit time decoders

--- src/main/scala/h8io/cfg/decoders/time.scala
+++ src/main/scala/h8io/cfg/decoders/time.scala
@@ -7,19 +7,20 @@ import h8io.cfg.Decoder.*
 import scala.concurrent.duration.Duration
 
 trait time {
-  val durationDecoder: Decoder[Duration] = stringDecoder.map(Duration(_))
+  implicit val durationDecoder: Decoder[Duration] = stringDecoder.map(Duration(_))
 
-  val javaDurationDecoder: Decoder[java.time.Duration] = stringDecoder.map(java.time.Duration.parse)
+  implicit val javaDurationDecoder: Decoder[java.time.Duration] = stringDecoder.map(java.time.Duration.parse)
 
-  val instantDecoder: Decoder[java.time.Instant] = stringDecoder.map(java.time.Instant.parse)
-  val localDateDecoder: Decoder[java.time.LocalDate] = stringDecoder.map(java.time.LocalDate.parse)
-  val localDateTimeDecoder: Decoder[java.time.LocalDateTime] = stringDecoder.map(java.time.LocalDateTime.parse)
-  val localTimeDecoder: Decoder[java.time.LocalTime] = stringDecoder.map(java.time.LocalTime.parse)
-  val monthDayDecoder: Decoder[java.time.MonthDay] = stringDecoder.map(java.time.MonthDay.parse)
-  val offsetDateTimeDecoder: Decoder[java.time.OffsetDateTime] = stringDecoder.map(java.time.OffsetDateTime.parse)
-  val offsetTimeDecoder: Decoder[java.time.OffsetTime] = stringDecoder.map(java.time.OffsetTime.parse)
-  val periodDecoder: Decoder[java.time.Period] = stringDecoder.map(java.time.Period.parse)
-  val yearDecoder: Decoder[java.time.Year] = stringDecoder.map(java.time.Year.parse)
-  val yearMonthDecoder: Decoder[java.time.YearMonth] = stringDecoder.map(java.time.YearMonth.parse)
-  val zonedDateTimeDecoder: Decoder[java.time.ZonedDateTime] = stringDecoder.map(java.time.ZonedDateTime.parse)
+  implicit val instantDecoder: Decoder[java.time.Instant] = stringDecoder.map(java.time.Instant.parse)
+  implicit val localDateDecoder: Decoder[java.time.LocalDate] = stringDecoder.map(java.time.LocalDate.parse)
+  implicit val localDateTimeDecoder: Decoder[java.time.LocalDateTime] = stringDecoder.map(java.time.LocalDateTime.parse)
+  implicit val localTimeDecoder: Decoder[java.time.LocalTime] = stringDecoder.map(java.time.LocalTime.parse)
+  implicit val monthDayDecoder: Decoder[java.time.MonthDay] = stringDecoder.map(java.time.MonthDay.parse)
+  implicit val offsetDateTimeDecoder: Decoder[java.time.OffsetDateTime] =
+    stringDecoder.map(java.time.OffsetDateTime.parse)
+  implicit val offsetTimeDecoder: Decoder[java.time.OffsetTime] = stringDecoder.map(java.time.OffsetTime.parse)
+  implicit val periodDecoder: Decoder[java.time.Period] = stringDecoder.map(java.time.Period.parse)
+  implicit val yearDecoder: Decoder[java.time.Year] = stringDecoder.map(java.time.Year.parse)
+  implicit val yearMonthDecoder: Decoder[java.time.YearMonth] = stringDecoder.map(java.time.YearMonth.parse)
+  implicit val zonedDateTimeDecoder: Decoder[java.time.ZonedDateTime] = stringDecoder.map(java.time.ZonedDateTime.parse)
 }
